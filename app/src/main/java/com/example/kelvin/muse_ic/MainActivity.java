a03e101130435c25b4367760e2ffb2a4466791c4
Kelvin Tam <kelvin.tam@live.com>
2016-02-27 17:53:31 -0500

    idk

--- app/src/main/java/com/example/kelvin/muse_ic/MainActivity.java
+++ app/src/main/java/com/example/kelvin/muse_ic/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.kelvin.muse_ic;
 
-import android.graphics.Color;
 import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,187 +9,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
-import android.widget.Toast;
 
-// SPOTIFY DEPENDENCIES
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerNotificationCallback;
 import com.spotify.sdk.android.player.PlayerState;
+
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.Config;
 
-// MYO DEPENDENCIES
-import com.thalmic.myo.AbstractDeviceListener;
-import com.thalmic.myo.Arm;
-import com.thalmic.myo.DeviceListener;
-import com.thalmic.myo.Hub;
-import com.thalmic.myo.Myo;
-import com.thalmic.myo.Pose;
-import com.thalmic.myo.Quaternion;
-import com.thalmic.myo.XDirection;
-import com.thalmic.myo.scanner.ScanActivity;
-
-
 public class MainActivity extends Activity implements
         PlayerNotificationCallback, ConnectionStateCallback {
-
-    TextView myoDebug;
-
-    // MYO Device Listener
-    private DeviceListener mListener = new AbstractDeviceListener() {
-
-        // onConnect() is called whenever a Myo has been connected.
-        @Override
-        public void onConnect(Myo myo, long timestamp) {
-            // Set the text color of the text view to cyan when a Myo connects.
-            myoDebug.setTextColor(Color.CYAN);
-        }
-
-        // onDisconnect() is called whenever a Myo has been disconnected.
-        @Override
-        public void onDisconnect(Myo myo, long timestamp) {
-            // Set the text color of the text view to red when a Myo disconnects.
-            myoDebug.setTextColor(Color.RED);
-        }
-
-        // onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
-        // arm. This lets Myo know which arm it's on and which way it's facing.
-        @Override
-        public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
-            myoDebug.setText("Myo synced");
-        }
-
-        // onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
-        // it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
-        // when Myo is moved around on the arm.
-        @Override
-        public void onArmUnsync(Myo myo, long timestamp) {
-            myoDebug.setText("Myo unsynced");
-        }
-
-        // onUnlock() is called whenever a synced Myo has been unlocked. Under the standard locking
-        // policy, that means poses will now be delivered to the listener.
-        @Override
-        public void onUnlock(Myo myo, long timestamp) {
-            myoDebug.setText("Myo unlocked");
-        }
-
-        // onLock() is called whenever a synced Myo has been locked. Under the standard locking
-        // policy, that means poses will no longer be delivered to the listener.
-        @Override
-        public void onLock(Myo myo, long timestamp) {
-            System.out.println("Myo locked");
-        }
-
-        // onOrientationData() is called whenever a Myo provides its current orientation,
-        // represented as a quaternion.
-        @Override
-        public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
-            // Calculate Euler angles (roll, pitch, and yaw) from the quaternion.
-            float roll = (float) Math.toDegrees(Quaternion.roll(rotation));
-            float pitch = (float) Math.toDegrees(Quaternion.pitch(rotation));
-            float yaw = (float) Math.toDegrees(Quaternion.yaw(rotation));
-
-            // Adjust roll and pitch for the orientation of the Myo on the arm.
-            if (myo.getXDirection() == XDirection.TOWARD_ELBOW) {
-                roll *= -1;
-                pitch *= -1;
-            }
-
-            // Next, we apply a rotation to the text view using the roll, pitch, and yaw.
-            myoDebug.setRotation(roll);
-            myoDebug.setRotationX(pitch);
-            myoDebug.setRotationY(yaw);
-        }
-
-        // onPose() is called whenever a Myo provides a new pose.
-        @Override
-        public void onPose(Myo myo, long timestamp, Pose pose) {
-            // Handle the cases of the Pose enumeration, and change the text of the text view
-            // based on the pose we receive.
-            switch (pose) {
-                case UNKNOWN:
-                    myoDebug.setText("Pose unknown");
-                    break;
-                case REST:
-                case DOUBLE_TAP:
-                    int restTextId = R.string.hello_world;
-                    switch (myo.getArm()) {
-                        case LEFT:
-                            myoDebug.setText("Double tap left");
-                            break;
-                        case RIGHT:
-                            myoDebug.setText("Double tap right");
-                            break;
-                    }
-                    myoDebug.setText(getString(restTextId));
-                    break;
-                case FIST:
-                    myoDebug.setText("Pose fist");
-                    break;
-                case WAVE_IN:
-                    myoDebug.setText("Pose wave in");
-                    break;
-                case WAVE_OUT:
-                    myoDebug.setText("Pose wave out");
-                    break;
-                case FINGERS_SPREAD:
-                    myoDebug.setText("Pose fingers spread");
-                    break;
-            }
-
-            if (pose != Pose.UNKNOWN && pose != Pose.REST) {
-                // Tell the Myo to stay unlocked until told otherwise. We do that here so you can
-                // hold the poses without the Myo becoming locked.
-                myo.unlock(Myo.UnlockType.HOLD);
-
-                // Notify the Myo that the pose has resulted in an action, in this case changing
-                // the text on the screen. The Myo will vibrate.
-                myo.notifyUserAction();
-            } else {
-                // Tell the Myo to stay unlocked only for a short period. This allows the Myo to
-                // stay unlocked while poses are being performed, but lock after inactivity.
-                myo.unlock(Myo.UnlockType.TIMED);
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        myoDebug = (TextView) findViewById(R.id.debug_label);
-        Hub hub = Hub.getInstance();
-
-        if (!hub.init(this, "com.example.kelvin.muse_ic")) {
-            // We can't do anything with the Myo device if the Hub can't be initialized, so exit.
-            Toast.makeText(this, "Couldn't initialize Hub", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-
-        // Next, register for DeviceListener callbacks.
-        hub.addListener(mListener);
-
-
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
                 REDIRECT_URI);
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
+
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
         createButtonListeners();
     }
-
 
     private void createButtonListeners() {
         ImageButton hButton = (ImageButton) findViewById(R.id.happyButton);
@@ -249,7 +100,6 @@ public class MainActivity extends Activity implements
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -273,7 +123,9 @@ public class MainActivity extends Activity implements
     SPOTIFY STARTS HERE
      */
 
+    // TODO: Replace with your client ID
     private static final String CLIENT_ID = "502e0cebcbbe4cd89668a1086a118533";
+    // TODO: Replace with your redirect URI
     private static final String REDIRECT_URI = "museic://callback";
 
     // Request code that will be passed together with authentication result to the onAuthenticationResult callback
@@ -346,19 +198,5 @@ public class MainActivity extends Activity implements
     protected void onDestroy() {
         Spotify.destroyPlayer(this);
         super.onDestroy();
-        super.onDestroy();
-        // We don't want any callbacks when the Activity is gone, so unregister the listener.
-        Hub.getInstance().removeListener(mListener);
-
-        if (isFinishing()) {
-            // The Activity is finishing, so shutdown the Hub. This will disconnect from the Myo.
-            Hub.getInstance().shutdown();
-        }
-        Hub.getInstance().removeListener(mListener);
-
-        if (isFinishing()) {
-            // The Activity is finishing, so shutdown the Hub. This will disconnect from the Myo.
-            Hub.getInstance().shutdown();
-        }
     }
 }
