0bfb379be13d36e2f9a4d87a974dd3d0c9157081
Jess <jdou@umich.edu>
2016-02-27 19:59:57 -0500

    helpful for moving muse stuff

---
+++ importMuse.sh
@@ -0,0 +1,4 @@
+cp ../TestLibMuseAndroid/app/src/main/res/values/strings.xml musehalp/app/src/main/res/values/strings.xml
+cp ../TestLibMuseAndroid/app/src/main/res/layout/activity_main.xml musehalp/app/src/main/res/layout/activity_main.xml
+cp ../TestLibMuseAndroid/app/src/main/java/com/interaxon/test/libmuse/MainActivity.java musehalp/app/src/main/java/com/interaxon/test/libmuse/MainActivity.java
+
